d840912e01642a14f15230f377a8b2eb1fe5fbe8
Valentin Drelon <63446112+Valentin-Droid@users.noreply.github.com>
2024-06-28 10:25:31 +0200

    Revert "Feature/prettier"

--- nba-api/nba-back/index.js
+++ nba-api/nba-back/index.js
@@ -1,13 +1,14 @@
-import express from "express";
-import { router as nbaRouter } from "./routes/nba.js";
-import { corsMiddleware } from "./middlewares.js";
-import "dotenv/config";
+import express from 'express';
+import { router as nbaRouter } from './routes/nba.js';
+import { corsMiddleware } from './middlewares.js';
+import 'dotenv/config';
+
 
 const app = express();
 const PORT = 3000;
 
 app.use(corsMiddleware);
-app.use("/nba", nbaRouter);
+app.use('/nba', nbaRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
